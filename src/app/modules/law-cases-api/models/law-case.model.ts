eb0b7594e8a7bcd73ef915252dd496cbd053747b
Joel Tatarka <joel423@gmail.com>
2020-03-09 22:38:54 -0400

    updates to nicer formatted name

--- src/app/modules/law-cases-api/models/law-case.model.ts
+++ src/app/modules/law-cases-api/models/law-case.model.ts
@@ -1,13 +1,13 @@
 export class LawCase {
   id: Number;
-  name: string;
+  name_abbreviation: string;
   decision_date: string;
   docket_number: string;
   url: string;
 
-  constructor(id: number, name: string, decision_date: string, docket_number: string, url: string) {
+  constructor(id: number, name_abbreviation: string, decision_date: string, docket_number: string, url: string) {
     this.id = id;
-    this.name = name;
+    this.name_abbreviation = name_abbreviation;
     this.decision_date = decision_date;
     this.docket_number = docket_number;
     this.url = url;
